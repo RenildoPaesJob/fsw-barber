4bf7f59da531fd09a298f768bd331efd0f8fe2c7
RenildoPaesJob <renildopaesjoob@gmail.com>
2024-08-12 08:57:40 -0300

    featÇ add api

--- src/app/_components/SidebarButton.tsx
+++ src/app/_components/SidebarButton.tsx
@@ -8,9 +8,11 @@ import {
 	SheetTitle
 } from "./ui/sheet"
 import { Button } from "./ui/button"
-import { Avatar, AvatarImage } from "./ui/avatar"
-import { CalendarIcon, HomeIcon, LogOutIcon } from "lucide-react"
+import { CalendarIcon, HomeIcon, LogInIcon } from "lucide-react"
 import { quickSearchOption } from "../_types/quickSearchOptions"
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "./ui/dialog"
+
+import GoogleIcon from "../../../public/assets/img/google_icon_solid.svg"
 
 export default function SidebarButton() {
 	return (
@@ -20,24 +22,35 @@ export default function SidebarButton() {
 			</SheetHeader>
 
 			{/* AVATAR */}
-			<div className="py-5 border-b border-solid flex items-center gap-2">
-				{/* <h2 className="font-bold text-lg">Olá faça seu login!</h2> */}
-				{/* <Dialog>
-					<DialogTrigger>
+			<div className="py-5 border-b border-solid flex justify-between items-center gap-2">
+				<h2 className="font-bold text-lg">Olá faça seu login!</h2>
+				<Dialog>
+					<DialogTrigger asChild>
 						<Button className="icon w-fit">
 							<LogInIcon />
 						</Button>
 					</DialogTrigger>
 
-					<DialogHeader>
-						<DialogTitle>Faça seu login!</DialogTitle>
-					</DialogHeader>
-				</Dialog> */}
-				<div className="w-10 h-10">
+					<DialogContent className="flex flex-col w-[90%]">
+						<DialogHeader>
+							<DialogTitle>Faça login na plataforma!</DialogTitle>
+							<DialogDescription>
+								Conecte-se usando sua conta do Google.
+							</DialogDescription>
+						</DialogHeader>
+
+						<Button variant="outline" className="flex gap-2 font-bold">
+							<Image src={GoogleIcon} alt="Fazer login com o google" />
+							Google
+						</Button>
+					</DialogContent>
+
+				</Dialog>
+				{/* <div className="w-10 h-10">
 					<Avatar>
 						<AvatarImage
 							alt="@shadcn"
-							src="https://images.unsplash.com/photo-1636041247094-a9e97be7a77a?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+							src="https://images.unsplash.com/photo-1706885093487-7eda37b48a60?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
 							className="object-cover rounded-full"
 						/>
 					</Avatar>
@@ -46,7 +59,7 @@ export default function SidebarButton() {
 				<div className="flex flex-col p-0">
 					<p className="text-xs">Renildo P.</p>
 					<p className="text-xs text-gray-400">renildop@gmail.com</p>
-				</div>
+				</div> */}
 			</div>
 
 			<div className="flex flex-col gap-2 py-5 border-b-2">
@@ -83,12 +96,12 @@ export default function SidebarButton() {
 			</div>
 
 			{/* LOGOUT */}
-			<div className="flex flex-col gap-2 py-5">
+			{/* <div className="flex flex-col gap-2 py-5">
 				<Button variant="ghost" className="flex gap-2">
 					<LogOutIcon />
 					Sair da conta
 				</Button>
-			</div>
+			</div> */}
 		</SheetContent>
 	)
 }
